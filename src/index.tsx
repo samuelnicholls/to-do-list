d5efd5651276e81afd0a12bdd1fc7240b19628ec
Samuel Nicholls <samuel.nicholls@hey.com>
2023-08-14 09:10:53 +0100

    Removing console error

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import './scss/app.scss'
 import App from './js/App'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+  </React.StrictMode>
+);
